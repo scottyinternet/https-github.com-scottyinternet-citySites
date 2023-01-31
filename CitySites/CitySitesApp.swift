7d1e31578e91d7f998d30cd9e3c136982fdc10cc
scottyinternet <griffin.scott88@gmail.com>
2023-01-31 17:04:34 -0600

    Initial Commit

---
+++ CitySites/CitySitesApp.swift
@@ -0,0 +1,17 @@
+//
+//  CitySitesApp.swift
+//  CitySites
+//
+//  Created by Scott Griffin on 1/31/23.
+//
+
+import SwiftUI
+
+@main
+struct CitySitesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
